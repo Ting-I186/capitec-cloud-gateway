84d53611820399e5b3d9fb564edad095d01b4452
Ting-I Liang <ting-i.liang@entelect.co.za>
2024-05-21 16:47:43 +0200

    Attempted to figure out routing, nothing worked

--- src/main/java/com/example/cloudGateway/CloudGatewayApplication.java
+++ src/main/java/com/example/cloudGateway/CloudGatewayApplication.java
@@ -2,6 +2,9 @@ package com.example.cloudGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -9,5 +12,13 @@ public class CloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("r1", r -> r.host("**.baeldung.com")
+						.and()
+						.path("/baeldung")
+						.uri("http://baeldung.com"))
+				.build();
+	}
 }
